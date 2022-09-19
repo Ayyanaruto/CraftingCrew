67e12ca972108853312f03e1e53b0921e675c5d4
Ayyanaruto <ayyan.shaikh.narotu@gmail.com>
2022-09-19 15:48:35 +0530

    COMMIT 12

--- utills/isAdmin.js
+++ utills/isAdmin.js
@@ -2,8 +2,8 @@ function isAdmin(req, res, next) {
     if (req.isAuthenticated() && (req.user.id == `${process.env.ADMIN}`)) {
         return next();
 
-    }
+    }else{
 
-    res.redirect('/admin/login')
+    res.redirect('/admin/login')}
 }
 module.exports=isAdmin
